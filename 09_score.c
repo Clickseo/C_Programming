5a76f23eaf178bd9d19c160bd05f67fb946c8ffb
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-25 21:41:49 +0900

    Update 09_score.c

--- 09_score.c
+++ 09_score.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 // 구조체 설계: SCORE(struct  _score)
-typedef  struct  _score {
+typedef struct _score {
 	char	name[12];
 	int	kor, eng, math, tot;
 	float	ave;
 } SCORE;
 
 // 함수 원형 선언
-void	input(SCORE* pScore);
-void	output(SCORE* pScore);
+void	input(SCORE *pScore);
+void	output(SCORE *pScore);
 
 int main(void)
 {
@@ -24,22 +24,20 @@ int main(void)
 }
 
 // 한 명의 학생 정보 입력
-void	input(SCORE* pS) {
+void	input(SCORE *p) {
 	printf("학생 성적을 입력 하세요. \n\n");
-	printf("학생 이름 : ");		gets_s(pS->name, sizeof(pS->name));
-	printf("국어 점수 : ");		scanf_s("%d", &pS->kor);
-	printf("영어 점수 : ");		scanf_s("%d", &pS->eng);
-	printf("수학 점수 : ");		scanf_s("%d", &pS->math);
+	printf("학생 이름: ");		gets_s(p->name, sizeof(pS->name));
+	printf("국어 점수: ");		scanf_s("%d", &p->kor);
+	printf("영어 점수: ");		scanf_s("%d", &p->eng);
+	printf("수학 점수: ");		scanf_s("%d", &p->math);
 
-	pS->tot = pS->kor + pS->eng + pS->math;
-	pS->ave = (float)pS->tot / 3;
-
-	return;
+	p->tot = p->kor + p->eng + p->math;
+	p->ave = (float)p->tot / 3;
 }
 
 // 한 명의 학생 정보 출력
-void	output(SCORE* pS) {
+void	output(SCORE *p) {
 	printf("\n\t ### 입력 정보 ### \n\n");
 	printf("%10s %3d %3d %3d %5d %8.2f \n",
-		pS->name, pS->kor, pS->eng, pS->math, pS->tot, pS->ave);
+		p->name, p->kor, p->eng, p->math, p->tot, p->ave);
 }
